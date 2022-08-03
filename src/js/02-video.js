82b3654c135350f2a574fd234ab73454596d049c
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-08-03 20:05:03 +0300

    2st task ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import { save, load } from './storage';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let savedPlayerTime = load('videoPlayer-current-time');
+
+const onPlay = time => save('videoPlayer-current-time', time);
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+player.setCurrentTime(savedPlayerTime.seconds);
