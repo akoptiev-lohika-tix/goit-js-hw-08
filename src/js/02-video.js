2e266c1e72f2cd615d68baed236c2274b2d492f9
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-08-08 10:08:42 +0300

    player fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,18 +3,20 @@ import _ from 'lodash';
 
 import { save, load } from './storage';
 
-let savedPlayerTime = load('videoPlayer-current-time');
-
-if (!savedPlayerTime) {
-  setInitialStorage();
-}
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+let savedPlayerTime = load('videoPlayer-current-time');
+
 const onPlay = time => save('videoPlayer-current-time', time.seconds);
 player.on('timeupdate', _.throttle(onPlay, 1000));
-player.setCurrentTime(savedPlayerTime);
+
+if (!savedPlayerTime) {
+  setInitialStorage();
+} else {
+  player.setCurrentTime(savedPlayerTime);
+}
 
 function setInitialStorage() {
   save('videoPlayer-current-time', 0);
